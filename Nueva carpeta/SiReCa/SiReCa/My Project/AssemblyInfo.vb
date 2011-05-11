3daf816aec35b67e2203c5c65ce8628ed336d078
23370 <23370@7282fd44-8c9d-4e51-a434-639f99fa0dbb>
2011-05-11 16:51:20 +0000

    V.0.

---
+++ Nueva carpeta/SiReCa/SiReCa/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La información general sobre un ensamblado se controla mediante el siguiente 
+' conjunto de atributos. Cambie estos atributos para modificar la información
+' asociada con un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+
+<Assembly: AssemblyTitle("SiReCa")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("SENER")> 
+<Assembly: AssemblyProduct("SiReCa")> 
+<Assembly: AssemblyCopyright("Copyright © SENER 2011")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'El siguiente GUID sirve como identificador de typelib si este proyecto se expone a COM
+<Assembly: Guid("0af3db03-60f3-42dc-bee8-66c5003e1a39")> 
+
+' La información de versión de un ensamblado consta de los cuatro valores siguientes:
+'
+'      Versión principal
+'      Versión secundaria 
+'      Número de versión de compilación
+'      Revisión
+'
+' Puede especificar todos los valores o establecer como predeterminados los números de versión de compilación y de revisión 
+' mediante el asterisco ('*'), como se muestra a continuación:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
